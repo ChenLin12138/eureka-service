f0aba6e54781c08b80684e908786999c17642b4f
ChenLin12138 <scorpion_chenlin@163.com>
2019-08-31 10:10:06 +0800

    解决Eureka客户端服务找不到Eureka Server的问题

--- src/main/java/com/lin/eureka/EurekaServiceApplication.java
+++ src/main/java/com/lin/eureka/EurekaServiceApplication.java
@@ -2,8 +2,11 @@ package com.lin.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//Spring服务中启动Eureka服务
+@EnableEurekaServer
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
